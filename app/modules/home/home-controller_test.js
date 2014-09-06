c8a7ca1a46ec0b9511297dc21dc7a9cf43ce0f6a
Steve Ayers <smaye81@gmail.com>
2014-09-06 17:23:52 -0400

    Added karma

---
+++ app/modules/home/home-controller_test.js
@@ -0,0 +1,10 @@
+define(function () {
+
+    describe('Home Controller Test', function () {
+
+        it('should pass', function () {
+            expect(true).toBe(true);
+        });
+        
+    });
+});
